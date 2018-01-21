3e0c195cab984dd353d0deb0f54da5ca54794469
MACJEEVAN <35652486+MACJEEVAN@users.noreply.github.com>
2018-01-21 15:51:58 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int b;
+clrscr();
+printf("the value of b is%d",b);
+scanf("%d",&b);
+b%2==0?printf("even number"):printf("odd number");
+getch();
+}
