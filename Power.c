d5487ee60d45390f1a43fdf956420a42bf3ce882
MACJEEVAN <35652486+MACJEEVAN@users.noreply.github.com>
2018-01-30 16:32:00 +0530

    Rename power.c to Power.c

---
+++ Power.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int d,e,f;
+clrscr();
+printf("enter the values of d and e");
+scanf("%d%d",&d,&e);
+f=pow(d,e);
+printf("the value of %d",f);
+getch();
+}
