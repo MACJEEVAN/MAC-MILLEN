04cf491a32fbb721932ab95aa1c83becfae8a929
MACJEEVAN <35652486+MACJEEVAN@users.noreply.github.com>
2018-02-10 12:07:28 +0530

    Create PRIME.c

---
+++ PRIME.c
@@ -0,0 +1,19 @@
+#include<conio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("enter the number ");
+scanf("%d",&a);
+printf("enter the another number ");
+scanf("%d",&b);
+for (c=a;c<=b;a++)
+{
+if (c%c=0 && c%num)
+{
+printf("% the prime number")
+}
+}
+getch();
+}
