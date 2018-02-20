564f6e64a40320a210621627a6e092fbfe78ea5d
MACJEEVAN <35652486+MACJEEVAN@users.noreply.github.com>
2018-02-20 16:29:19 +0530

    Create twotimes.c

---
+++ twotimes.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int h,h1,h2,m1,m2,m;
+clrscr();
+printf("Enter the value of hours");
+scanf("%d%d",&h1,&h2);
+printf("Enter the minutes");
+scanf("%d%d",&m1,&m2);
+h=h1-h2;
+m=m1-m2;
+printf("the subtracted value is %d %d",h,m);
+getch();
+}
