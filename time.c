d85a51836234963007d1e02658fe41f718f88b4a
MACJEEVAN <35652486+MACJEEVAN@users.noreply.github.com>
2018-02-20 16:20:32 +0530

    Create time.c

---
+++ time.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float h,m;
+clrscr();
+printf("Enter the value of time in minutes");
+scanf("%f",&m);
+h=m/60;
+printf("hour is %f",h);
+getch();
+}
