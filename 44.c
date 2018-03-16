f8aae608c9a78e67995efc4252f608b8d4a6c351
MACJEEVAN <35652486+MACJEEVAN@users.noreply.github.com>
2018-03-16 14:43:00 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) 
+{
+	int num,i;
+	scanf("%d",&num);
+	for(i=1;i<=10;i++)
+	{
+		if(num==i)
+		{
+			printf("Yes");
+		
+	     }
+	}
+	return 0;
+}
