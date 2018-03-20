eebaf615cc35995b676ff0fa0cf7448720da1426
MACJEEVAN <35652486+MACJEEVAN@users.noreply.github.com>
2018-03-20 16:11:01 +0530

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void) 
+{
+  int a[100],n,sum=0,i,avg=0;
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  scanf("%d",&a[i]);
+  for(i=0;i<n;i++)
+{  
+  sum=sum+a[i];
+}
+  avg=sum/n;
+  printf("%d",avg);
+  return 0;
+}
